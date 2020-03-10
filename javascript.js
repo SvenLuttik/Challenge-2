b2714153a29652fc5853a9de980500de526af76c
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-03-10 23:27:10 +0100

    Cleanup
    ziet er ff wat netter uit zo

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
-var now = new Date()
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    var currentTime = h + ":" + m + ":" + s + " ";
+    document.getElementById("time").innerText = currentTime;
+    document.getElementById("time").textContent = currentTime;
+    
+    setTimeout(showTime, 1000);
+    
+}
 
-document.getElementById('time').innerHTML= now ;
-
-console.log(now);
+showTime();
