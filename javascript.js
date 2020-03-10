eb589de08d9e4c1286c3e010dc4126afbd0d76dd
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-03-11 00:16:40 +0100

    Update .date
    datum, klok en seconde wijzer doen het nu

--- javascript.js
+++ javascript.js
@@ -1,15 +1,34 @@
+//De code voor de uren en minuten digitaal begint hier
 function showTime(){
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
-    var s = date.getSeconds();
+    var d = new Date();
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     
-    var currentTime = h + ":" + m + ":" + s + " ";
-    document.getElementById("time").innerText = currentTime;
-    document.getElementById("time").textContent = currentTime;
+    document.getElementById("dateToday").innerHTML = days[d.getDay()];
+    document.getElementById("timeHour").textContent = h;
+    document.getElementById("timeMinute").textContent = m;
     
     setTimeout(showTime, 1000);
     
 }
 
 showTime();
+
+//De code voor de seconden in anoloog begint hier
+var secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  var currentDate = new Date()
+  var secondsRatio = currentDate.getSeconds() / 60
+  setRotation(secondHand, secondsRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setInterval(setClock, 1000)
+
+setClock()
